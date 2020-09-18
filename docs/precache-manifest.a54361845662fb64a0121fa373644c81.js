eca0cd5bc9ba49abb20314cc289fb6b30d25101c
guimainini <guimaini@gmail.com>
2020-09-18 14:37:57 -0300

    2er comentario , docs agregado

---
+++ docs/precache-manifest.a54361845662fb64a0121fa373644c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24b3a5d4d80206efd88ed1b70b24764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d67c8ff7f6c3882617d3",
+    "url": "/static/css/main.fd82a310.chunk.css"
+  },
+  {
+    "revision": "a548f23b8cdd237daf14",
+    "url": "/static/js/2.0e55909f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0e55909f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d67c8ff7f6c3882617d3",
+    "url": "/static/js/main.bff32670.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
